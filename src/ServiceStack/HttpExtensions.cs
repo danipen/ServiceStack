55da94885c18f853e5aa397f86da12d11f1f9edd
danipen <danipen@.(none)>
2013-03-11 17:18:28 +0100

    Change in branch plastic

--- src/ServiceStack/HttpExtensions.cs
+++ src/ServiceStack/HttpExtensions.cs
@@ -18,6 +18,7 @@ namespace ServiceStack
         public static HttpRequestContext ToRequestContext(this HttpContext httpContext, object requestDto = null)
         {
             return new HttpRequestContext(
+                // vamos que nos vamos
                 httpContext.Request.ToRequest(),
                 httpContext.Response.ToResponse(),
                 requestDto);
